d23a0843841b5cff577a13db29fa66bf758f481d
Rasyadi Abdoellah <rfabdoellah@gmail.com>
2021-01-22 18:23:17 +0700

    Prototype desktop scroll logic - Move nav path defs to array in App - Change nav to render array passed in props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import { Switch, Route } from 'react-router-dom'
+import React from 'react'
+import { Switch, Route, useLocation, useHistory } from 'react-router-dom'
 import Nav from 'components/Nav'
 
-function App() {
-  return (
 
+
+function App() {
+  const history = useHistory()
+  let location = useLocation()
+  let current
+  
+  const navList = [
+    {path: '/', content: 'Hi!', altContent: 'Top'},
+    {path: '/projects', content: 'Projects'},
+    {path: '/skills', content: 'Skills'},
+    {path: '/me', content: 'About'}
+  ]
+  
+  for( const index in navList ){
+    if(navList[index].path === location.pathname){
+      current = parseInt(index)
+    }
+  } 
+  
+  //wheelAction requires useCallback because the current calue is not updated on rerender.
+  //The theory is that once called wheelAction runs once then the entire component is rerendered, however the values inside the function are not redefined.
+  const wheelAction = React.useCallback((event) => {
+    if(event.deltaY < 0 && current !== 0) {
+      history.push(navList[current-1].path)
+    }
+    if(event.deltaY > 0 && current < navList.length-1) {
+      history.push(navList[current+1].path)
+
+    }
+    // eslint-disable-next-line
+  }, [current])
+  
+  
+  React.useEffect(() => {
+    window.addEventListener('wheel', wheelAction)
+    return () => {
+      window.removeEventListener('wheel', wheelAction)
+    }
+  }, [wheelAction])
+
+  return (
     <>
-      <Nav/>  
+      <Nav list={navList}/>  
       <Switch>
         <Route exact path="/">
           <header className="main-banner">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import './Nav.css'
 
-export default function Nav() {
+export default function Nav({list}) {
+  let { pathname } = useLocation()
   return (
-  <nav className="fixed-nav">
-    <NavLink exact to="/">
-      Top
-    </NavLink>
-    <NavLink to="/projects">
-      Projects
-    </NavLink>
-    <NavLink to="/skills">
-      Skills
-    </NavLink>
-    <NavLink to ="/me">
-      About
-    </NavLink>
+    <nav className="fixed-nav">
+      {list.map((entry, i) => {
+        let navContent = entry.content
+
+        if(entry.altContent){
+          pathname === entry.path ? navContent = entry.content : navContent = entry.altContent
+        }
+
+        return (
+          <NavLink exact to={entry.path} key={i+1}>
+            {navContent}
+          </NavLink>
+        )
+      })}
   </nav>
   )
 }
