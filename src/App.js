6439d6d432aeea8e4079e787dcee9a62268206e7
Rasyadi Abdoellah <rfabdoellah@gmail.com>
2021-01-30 12:38:09 +0700

    Add conditionals in effects and create page components - change useEffect to useLayoutEffect to trigger them earlier - add condition checks in useLayoutEffects - add direction ref to determine scroll direction - Create individual page components and import into App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,67 @@
 import React from 'react'
-import { Switch, Route, useLocation, useHistory } from 'react-router-dom'
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom'
 import {WheelHandler, TouchStartHandler, TouchMoveHandler} from 'Handlers'
 
 import Nav from 'components/Nav'
-import Container from 'components/Container'
+import Intro from 'components/Intro'
+import Projects from 'components/Projects'
+import Skills from 'components/Skills'
+import Contact from 'components/Contact'
 import 'App.css'
+
+const navList =  [
+  {path: '/', content: 'Hi!', altContent: 'Home'},
+  {path: '/projects', content: 'Projects'},
+  {path: '/skills', content: 'Skills'},
+  {path: '/me', content: 'About'}
+]
 
 function usePrevious(value) {
   const ref = React.useRef();
   React.useEffect(() => {
+    // console.log('prev value effect')
     ref.current = value;
-  });
+  }, [value]);
   return ref.current;
 }
 
 function App() {
-  
-  let location = useLocation()
+  // console.log('paint')
   const history = useHistory()
-  
-  const navList = [
-    {path: '/', content: 'Hi!', altContent: 'Top'},
-    {path: '/projects', content: 'Projects'},
-    {path: '/skills', content: 'Skills'},
-    {path: '/me', content: 'About'}
-  ]
-  
+  const location = useLocation()
+
   //sets current to index of current path and stores previous current state in prev
-  const [current, setCurrent] = React.useState(navList.map(e => e.path).indexOf(location.pathname))
+  const [current, setCurrent] = React.useState(navList.map(e => e.path).indexOf(history.location.pathname))
   const prev = usePrevious(current)
+  const direction = React.useRef()
+  
+  //sync current value with index of navList item when location changes
+  React.useLayoutEffect(() => {
+    // console.log('location effect')
+    //only set current if it differs from location index
+    if(current !== navList.map(e => e.path).indexOf(location.pathname)){
+      // console.log('set current from location')
+      setCurrent(navList.map(e => e.path).indexOf(location.pathname))
+    }
 
+    //eslint-disable-next-line
+  }, [location])
 
-  React.useEffect(() => {
-    setCurrent(navList.map(e => e.path).indexOf(location.pathname))
-  }, [location.pathname])
+  //push history state when current value changes
+  React.useLayoutEffect(() => {
+    // console.log('current effect')
+    direction.current = current - prev
+    //only push to history if current does not match location index
+    if(current !== navList.map(e => e.path).indexOf(location.pathname)){
+      // console.log('set history from current')
+      history.push(navList[current].path)
+    }
+    //eslint-disable-next-line
+  },[current])
 
   //add listener after screen is set
   React.useEffect(() => {
+    // console.log('in listener set')
     window.addEventListener('wheel', WheelHandler(setCurrent, (navList.length-1), 250), {passive: false})
     window.addEventListener('touchstart', TouchStartHandler, {passive: false})
     window.addEventListener('touchmove', TouchMoveHandler(setCurrent, (navList.length-1), 330), {passive: false})
@@ -48,37 +73,22 @@ function App() {
     }
     // eslint-disable-next-line
   },[])
-  //push history state when current value changes
-  React.useEffect(() => {
-    // console.log(current - prev)
-    history.push(navList[current].path)
-    //eslint-disable-next-line
-  },[current])
-
 
   return (
     <>
       <Nav list={navList}/>
       <Switch>
         <Route exact path="/">
-          <Container fullscreen style={{backgroundColor: '#fff'}}>
-            Top
-          </Container>
+          <Intro/>
         </Route>
         <Route path="/projects">
-          <Container fullscreen style={{backgroundColor: '#ddd'}}>
-            projects
-          </Container>
+          <Projects/>
         </Route>
         <Route path="/skills">
-          <Container fullscreen style={{backgroundColor: '#aaa'}}>
-            skills
-          </Container>
+          <Skills/>
         </Route>
         <Route path="/me">
-          <Container fullscreen style={{backgroundColor: '#555'}}>
-            about + contact
-          </Container>
+          <Contact/>
         </Route>
       </Switch>  
     </>
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Container from 'components/Container'
+
+export default function Top(props) {
+  return (
+    <Container fullscreen style={{backgroundColor: '#fff'}}>
+    Hello! I'm Ras
+    </Container>
+  )
+}
