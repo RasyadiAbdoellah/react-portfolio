2973207cd09db65721e1b5a0a59fd302146ca4b4
Rasyadi Abdoellah <rfabdoellah@gmail.com>
2021-05-20 17:15:59 +0700

    add md parsing and animations to projects - add react-markdown package - add md file loader and parsing - add animations to parsed md components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,52 @@
 import React from 'react'
 import {motion as m, AnimateSharedLayout} from 'framer-motion'
+import ReactMarkdown from 'react-markdown'
 
 import Container from 'components/Container'
+import './Projects.css'
+
+const importAll = (r) => r.keys().map(r)
+const mdFiles = importAll(require.context('md/', false, /\.md$/))
+
+const cardAnim = {
+  enter: {
+    opacity: 0,
+    y: 50
+  },
+  center: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: 50,
+    transition: {
+      opacity: { duration: 0.2}
+    }
+  },
+}
 
 export default function Projects (props) {
+  
+  const [contentList ,setContentList] = React.useState(null)
+
+  React.useLayoutEffect(() => {
+    async function fetchTexts() {
+      const texts = await Promise.all(mdFiles.map(file => fetch(file.default).then(res => res.text()).catch(err => console.error(err))))
+      setContentList(texts)
+    }
+    fetchTexts()
+  }, [])
+  
   return (
-    <Container className="projects">
-      projects
+    contentList && <Container className="projects">
+        {contentList.map((content, i) => {
+          return (
+            <m.div key={i} variants={cardAnim} className="card">
+              <ReactMarkdown children={content}/>
+            </m.div>
+          )
+        })}
     </Container>
   )
 }
