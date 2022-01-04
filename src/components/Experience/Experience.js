be67b6fd93e9a3c030e1117145c6490de9e50ca6
Ras Abdoellah <rfabdoellah@gmail.com>
2022-01-04 08:25:04 -0500

    Move icons list to iconlist component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,6 +4,7 @@ import { motion as m } from 'framer-motion'
 import { Helmet } from 'react-helmet'
 
 import Container from 'components/Container'
+import IconList from 'components/IconList'
 import './Experience.css'
 
 const cardAnim = {
@@ -42,28 +43,43 @@ const activeContentAnim = {
   }
 }
 
-const fragment = (     
-<div>
-  <h1>Tools I Use</h1>
-  <ul>
-    <li>
-    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xd/xd-plain.svg"  alt="Adobe XD" title="Adobe XD"/>
-    </li>
-    <li>
-    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt="Figma" title="Figma"/>
-    </li>
-    <li>
-    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" alt="VS Code" title="VS Code"/>
-    </li>
-    <li>
-    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" alt="Git" title="Git"/>
-    </li>
-  </ul>
-</div>
-)
 
 export default function Experience (props) {
+
   const [current, setCurrent] = React.useState(0)
+  
+  const FEListItems = ["Bootstrap", "Bulma", "React", "Redux", "Gatsby", "Storybook"]
+  const BEListItems = [
+    "NodeJS", 
+    [
+      "Express",
+      {
+        className: "devicon-express-original",
+        color: false
+      }
+    ], 
+    "Sequelize", 
+    [
+      "Ruby on Rails", 
+      {
+        className: "devicon-rails-plain",
+        color: true
+      }
+    ], 
+    ["MySQL", { color: false }], 
+    "PostgreSQL"
+  ]
+  const toolsList = [
+    [
+      "AWS",
+      {
+        className: "devicon-amazonwebservices-plain"
+      }
+    ],
+    "Heroku",
+    "Docker"
+  ]
+
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark(
@@ -122,63 +138,15 @@ export default function Experience (props) {
           <div className='inner'>
             <div>
               <h2>Frontend</h2>
-              <ul className='icon-list'>
-                <li>
-                  <i className="devicon-bootstrap-plain colored"></i> Bootstrap
-                </li>
-                <li>
-                  <i className="devicon-bulma-plain colored"></i> Bulma
-                </li>
-                <li>
-                  <i className="devicon-react-original colored"></i> React
-                </li>
-                <li>
-                  <i className="devicon-redux-original colored"></i> Redux
-                </li>
-                <li>
-                  <i className="devicon-gatsby-plain colored"></i> Gatsby
-                </li>
-                <li>
-                  <i class="devicon-storybook-plain colored"></i> Storybook
-                </li>
-              </ul>
+              <IconList list={FEListItems}/>
             </div>
             <div>
               <h2>Backend</h2>
-              <ul className='icon-list'>
-                <li>
-                  <i className="devicon-nodejs-plain colored" title='NodeJS'></i> NodeJS
-                </li>
-                <li>
-                  <i className="devicon-express-original" title='Express.js'></i> Express
-                </li>
-                <li>
-                  <i className="devicon-sequelize-plain colored" title='Sequelize'></i> Sequelize
-                </li>
-                <li>
-                  <i className="devicon-rails-plain colored" title='Ruby on Rails'></i> Ruby on Rails
-                </li>
-                <li>
-                  <i className="devicon-mysql-plain" title='MySQL'></i> MySQL
-                </li>
-                <li>
-                  <i className="devicon-postgresql-plain colored" title='PostgreSQL'></i> Postgres
-                </li>
-              </ul>
+              <IconList list={BEListItems}/>
             </div>
             <div>
               <h2>Tools</h2>
-              <ul className='icon-list'>
-                <li>
-                  <i className="devicon-amazonwebservices-plain colored" title='AWS'></i> AWS
-                </li>
-                <li>
-                  <i className="devicon-heroku-plain colored" title='Heroku'></i> Heroku
-                </li>
-                <li>
-                  <i className="devicon-docker-plain colored" title='Docker'></i> Docker
-                </li>
-              </ul>
+              <IconList list={toolsList}/>
             </div>
           </div>
         </div>
