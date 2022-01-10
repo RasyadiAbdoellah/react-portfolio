29ff619b0b189586eacc0888b1a9aca0ba2fb476
Rasyadi Abdoellah <rfabdoellah@gmail.com>
2022-01-10 05:11:01 -0500

    fix meta description and add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import Experience from 'components/Experience'
 import Work from 'components/Work'
 import Contact from 'components/Contact'
 
+import logo from 'logo.svg'
+
 const navList =  [
   {path: '#top', content: 'Hi!'},
   {path: '#me', content: 'About'},
@@ -18,8 +20,10 @@ function App() {
   return (
     <>
       <Helmet>
+        <html lang='en' />
         <title>Rasyadi Abdoellah | Web Developer & UX Designer</title>
-        <description>Designer turned developer with a love for building thoughtful, intuitive experiences.</description>
+        <meta name='description' content='Designer turned developer with a love for building thoughtful, intuitive experiences.' />
+        <link rel='icon' href={logo} />
       </Helmet>
       <Nav list={navList}/>
       <Intro/>
